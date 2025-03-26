4742bc1656d2f25e2e2551a844df5ee661e61468
aoge2716 <aoge2716@gmail.com>
2025-03-26 04:11:23 +0100

    modified styling and setup for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import NavBar from './components/NavBar'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
       <NavBar/>
-      <Routes>
-        <Route
-          path='/'
-          element={<HomePage/>}
-        />
-        
-      </Routes>
+      <div className='body'>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage/>}
+          />
+          
+          <Route
+            path='/signup'
+            element={<SignupPage/>}
+          />
+          
+          <Route
+            path='/login'
+            element={<LoginPage/>}
+          />
+          
+        </Routes>
+      </div>
+      
     </>
   )
 }
